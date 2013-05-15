275ba284c1aefa82ff68ce3d7fb55cb7aa94a08e
Hannes Diercks <info@xiphe.net>
2013-05-15 23:04:20 +0200

    add documention

--- src/namespace.js
+++ src/namespace.js
@@ -1,29 +1,54 @@
-var namespace = this.namespace = function(space, value, enableOverwrite) {
-  var self = this, object = this, tokens = space.split("."), token, overwrite = false;
+var namespace = this.namespace = function(space, value, overwrite) {
+  var undef,
+    token,
+    self = this,
+    object = self,
+    undef_str = typeof undef,
+    tokens = space.split('.');
 
-  if (typeof enableOverwrite !== 'undefined') {
-    overwrite = Boolean(enableOverwrite);
-  }
+  /* Turn off overwriting by default. */
+  overwrite = (typeof overwrite !== undef_str ? overwrite : false);
 
+  /* Loop through tokens. */
   while (tokens.length > 0) {
     token = tokens.shift();
 
-    if (tokens.length > 0 && typeof object[token] !== 'object' && typeof object[token] !== 'function' && typeof object[token] !== "undefined") {
+    /*
+     * If this is a middle token and is not capable to handle sub objects
+     * throw an error and return undefined.
+     */
+    if (tokens.length > 0 &&
+      typeof object[token] !== 'object' &&
+      typeof object[token] !== 'function' &&
+      typeof object[token] !== undef_str
+    ) {
       if (self.console) {
         self.console.error('Namespace token "'+token+'" is not an object or function.');
       }
-      return false;
+      return undef;
     }
 
-    if (typeof object[token] === "undefined") {
-      if (typeof value === 'undefined') {
-        return;
+    /* If the token is not defined jet... */
+    if (typeof object[token] === undef_str) {
+      /* And no value is passed... */
+      if (typeof value === undef_str) {
+        /* Stop looping and return undefined. */
+        return undef;
       }
+
+      /*
+       * If this is a middle token, make it a new object.
+       * If it is the last one - set the value.
+       */
       object[token] = tokens.length === 0 ? value : {};
-    } else if(tokens.length === 0 && overwrite) {
+    }
+
+    /* If this is the last token and, it is defined and overwriting is enabled. */
+    else if(tokens.length === 0 && overwrite) {
       object[token] = value;
     }
 
+    /* Get deeper. */
     object = object[token];
   }
 
