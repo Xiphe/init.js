ad9cae966c8c251c392f4e347c4762270322e1b8
Hannes Diercks <info@xiphe.net>
2013-05-16 02:22:16 +0200

    finetune waituntil and more documentation.

--- README.md
+++ README.md
@@ -4,10 +4,84 @@ xiphe-init.js
 Basic js functions for namespacing, loose/asynchronous dependencies, and jQuery plugins.
 
 
+Namespace
+---------
+> _mixed_ **namespace** ( _string_ objectPath [, _mixed_ value ] [, _boolean_ overwrite = false ] );
+
+---
+
+#### Case 1: Set a value into a nested object.
+```js
+var ipsm = namespace('foo.bar.lorem', 'ipsum');
+```
+Is short for:
+
+```js
+var ipsm = 'ipsum';
+window.foo = {
+  bar: {
+    lorem: ipsm
+  }
+};
+```
+But existent values will not be overwritten and existent objects will be extended.
+Overwriting is disabled by default. To replace 'ipsum' with 'dolor' the third parameter has to be true.
+```js
+namespace('foo.bar.lorem', 'dolor', true);
+```
+
+#### Case 2: Get a value from a nested object.
+```js
+namespace('foo.bar.lorem');
+```
+Will return "ipsum".
+```js
+namespace('not.set.jet');
+```
+Will return undefined but will not throw an error if not or set are not defined jet.
+
+#### Error Case 1: Don't use middle tokens that are not objects or functions.
+```js
+var foo = 'This is not an object.';
+namespace('foo.bar', 'lorem');
+```
+This will throw an error and return undefined because foo is not an object or function.
+Passing true as the third parameter will not suppress this error - you should handle that by yourself. 
+
+
+Wait Until
+----------
+> _undefined_ **xiphe.wait.until** ( _string_ module, _function_ callback [, _integer_ intervall = 10 ] [, _integer_ patience = 3000 ] [, _boolean_ continue_on_error = true] );
+
+---
+
+This function is collecting `callbacks` that will be fired once the `module` stops being undefined. If the module is allready defined, the callback is fired instantly.  
+This comes handy when dealing with asynchronous loading and loose dependencies.
+
+#### Example
+```js
+var foo;
+xiphe.wait.until('foo', function(module) {
+  console.log("Foo is now available and it's value is "+module);
+});
+window.setTimeout(function() { foo = 'bar'; }, 100);
+```
+This will print "Foo is now available and it's value is bar" into the console after 100 miliseconds.
+
+#### Intervall
+In the background, a loop is started for each module. The time between two checks can be adjusted by setting the third value.  
+```js
+xiphe.wait.until('foo', function(module) {
+  console.log("Foo is now available");
+}, 1000);
+var foo = 'bar';
+```
+"Foo is now available" will be printed after one second.
+
+
 Todo
 ----
-
-* Write Documentation O_°
+* more documentation.
 
 
 License
--- src/waituntil.js
+++ src/waituntil.js
@@ -1,47 +1,123 @@
 /*global namespace, xiphe */
 namespace('xiphe.wait.patience', 3000);
 namespace('xiphe.wait.until', (function() {
-  var undef, _available, _callbacks = {}, _waitFor, self = this;
+  var undef,
+    _available,
+    _waitFor,
+    _callbacks,
+    self = this,
+    undef_str = typeof undef,
+    _callback_list = {};
 
+  /**
+   * Extension of the namespace function.
+   * Uses _callback_list instead of window object.
+   *
+   * @param  {String}  objectPath
+   * @param  {Mixed}   variable
+   * @param  {Boolean} overwrite
+   *
+   * @return {Mixed}   Whatever is at the end of the objectPath.
+   */
+  _callbacks = function() { return namespace.apply(_callback_list, arguments); };
+
+  /**
+   * Check if the passed module is defined yet.
+   *
+   * @param  {string} module the module name
+   *
+   * @return {boolean}
+   */
   _available = function(module) {
-    return (typeof namespace(module) !== 'undefined');
+    return (typeof namespace(module) !== undef_str);
   };
 
-  _waitFor = function(module, intervall, patience, continue_on_error) {
-    var h = new Date().getTime(),
+  /**
+   * Initiate a new loop that waits for a module to become available.
+   *
+   * @param  {String}  module          The module name.
+   * @param  {Integer} interval       The pause between checks.
+   * @param  {Integer} patience        The time until abort.
+   * @param  {Boolean} continueOnError Whether or not the callbacks should be fired if loop was aborted.
+   *
+   * @return {Undefined}
+   */
+  _waitFor = function(module, interval, patience, continueOnError) {
+    var endOfPartience = new Date().getTime(),
+      moduleKey = Array.prototype.slice.call(arguments).join('.'),
       error = false,
       _loader;
 
-    intervall =+ (typeof intervall === 'undefined' ? 10 : intervall);
-    patience =+ (typeof patience === 'undefined' ? xiphe.wait.patience : patience);
-    continue_on_error = (typeof continue_on_error === 'undefined' ? true : Boolean(continue_on_error));
+    /* Set defaults for patience and continueOnError if not defined. */
+    endOfPartience += patience =+ (typeof patience === undef_str ? xiphe.wait.patience : patience);
+    continueOnError = (typeof continueOnError === undef_str ? true : continueOnError);
 
     _loader = self.setInterval(function() {
+      /* If module became available or patience is over. */
       if (_available(module) ||
-        (patience > -1 && h + patience <= new Date().getTime() && (error = true) === true)
+        (patience > -1 && endOfPartience <= new Date().getTime() && (error = true) === true)
       ) {
+        var _myCallbacks;
+
+        /* Stop the timer */
         self.clearInterval(_loader);
+
+        /* Notify the console if the timer stopped because patience was over. */
         if (error && self.console) {
           self.console.error('Module "'+module+'" not found after '+(patience/1000)+' seconds - aborting!');
         }
-        if (!error || continue_on_error) {
-          for (var i = 0, l = _callbacks[module].length; i < l; i++) {
-            _callbacks[module][i].call(self, namespace(module));
+
+        /* Fire callbacks if no error occurred or callbacks should be fired anyways. */
+        if (!error || continueOnError) {
+          _myCallbacks = _callbacks(moduleKey);
+          for (var i = 0, l = _myCallbacks.length; i < l; i++) {
+            _myCallbacks[i].call(self, namespace(module));
           }
         }
-        _callbacks[module] = undef;
+
+        /* Clear the callback list. */
+        _callbacks(moduleKey, undef, true);
       }
-    }, intervall);
+    }, interval);
   };
 
-  return function(module, callback, intervall, patience, continue_on_error) {
+  /**
+   * The actual interface
+   *
+   * @param  {String}   module          The module name.
+   * @param  {Function} callback        The callback function.
+   * @param  {Integer}  interval        The pause between checks.
+   * @param  {Integer}  patience        The time until abort.
+   * @param  {Boolean}  continueOnError Whether or not the callbacks should be fired if loop was aborted.
+   *
+   * @return {Undefined}
+   */
+  return function(module, callback, interval) {
+    var moduleKey, _arguments = Array.prototype.slice.call(arguments);
+
+    /* Set the default on the interval if undefined. */
+    _arguments[2] = intervall =+ (typeof intervall === undef_str ? 10 : interval);
+
+    /* If the module is unavailable... */
     if (!_available(module)) {
-      if (typeof _callbacks[module] === 'undefined') {
-        _callbacks[module] = [];
-        _waitFor(module, intervall, patience, continue_on_error);
+      /* Build the full key from arguments without callback. */
+      _arguments.splice(1,1);
+      moduleKey = _arguments.join('.');
+
+      /*
+       * If this is the first callback for this module prepare the
+       * callback list and start a new loop.
+       */
+      if (typeof _callbacks(moduleKey) === undef_str) {
+        _callbacks(moduleKey, []);
+        _waitFor.apply(self, _arguments);
       }
-      _callbacks[module].push(callback);
-    } else {
+
+      /* And add the callback to the list. */
+      _callbacks(moduleKey).push(callback);
+    }
+    /* Fire the callback instantly if the module is already defined. */
+    else {
       callback.call(self, namespace(module));
     }
   };
